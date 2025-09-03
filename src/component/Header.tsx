5be0c9095718d85484df6e6df40b5efd3df5bd6c
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-09-03 10:15:07 +0200

    feature:Change to dev branch also done with first component

---
+++ src/component/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../assets/logo.svg"
+import headerBg from "../assets/bg-header-desktop.png"
+
+
+export default function Header() {
+  return (
+<div className='header' style={{width:"100%", height:"15%", border:"1px solid red", backgroundImage:`url(${headerBg})`, display:"flex", justifyContent:"center",alignItems:"center"}}>
+<img src={logo} alt="logo" />
+</div>
+  
+  );
+}
+
---
+++ src/component/HeroSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div>
+      <h1>A History Of Everything You Copy</h1>
+      <p></p>
+    </div>
+  )
+}
